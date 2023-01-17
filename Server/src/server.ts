a8d56ebb82b2068ccd3b785b6bb77690d9a89305
Charles <charles09111999@gmail.com>
2023-01-17 08:58:17 -0300

    update: finished half aula 01

---
+++ Server/src/server.ts
@@ -0,0 +1,30 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+/* 
+    _> Metodos HTTP: 
+    Get(Buscar alguma informação), 
+    Post(Criar alguma coisa), 
+    Put(Atualizar um recurso por completo), Patch(Atualizar um recurso especifico), Delete(Deletar um recurso dentro do Back-end);
+
+*/
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany()
+
+
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log("HTTP Server Running!")
+})
